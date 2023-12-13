24b644ea31d536b408c12308cc5d72b00ce34a69
Biplove Jaisi <200572872@student.georgianc.on.ca>
2023-12-13 11:12:33 -0500

    Most of the columns from data got added

--- app/api/data_transformer.py
+++ app/api/data_transformer.py
@@ -1,4 +1,4 @@
-# app/api/data_transformer.py
+# Import Required Libraries
 from flask import Blueprint, jsonify, current_app
 from bson import json_util
 
@@ -11,11 +11,23 @@ def parse_json(data):
 def get_all_items():
     # Project only the specific fields
     projection = {
+        "id": 1,
         "case_number": 1,
         "date": 1,
+        "block": 1,
+        "iucr": 1,
         "primary_type": 1,
+        "description": 1,
         "location_description": 1,
         "arrest": 1,
+        "domestic": 1,
+        "beat": 1,
+        "district": 1,
+        "ward": 1,
+        "community_area": 1,
+        "fbi_code":1,
+        "year": 1,
+        "updated_on": 1,
         "_id": 0  # Exclude the default _id field
     }
 
